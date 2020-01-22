e47d1d7682c4b8f9c640fcbf454dd288a1a9feae
sAnjali12 <anjalisen060@gmail.com>
2020-01-22 16:15:47 +0530

    createed login route and verify json web token

--- routes/registeration.js
+++ routes/registeration.js
@@ -12,12 +12,34 @@ register.post("/registeration",(req,res)=>{
    }
    let Response = registerDb.insertData(userDetail)
    Response.then((data)=>{
-    let token = jwt.sign({"user":data},"Anjalis")
-    res.cookie(token)
     res.json("posted..")
    }).catch((err)=>{
        res.send(err)
    })
 });
+
+register.get("/login",(req, res)=> {
+    var email = req.body.email;
+    var password = req.body.password;
+    var Response = registerDb.user_login(email)
+    Response.then((data)=>{
+        if(data.length == 0){
+            res.send({massage: "email is invalid.."})
+        }
+        else if(password == data[0]['password']){
+            let token = jwt.sign({"user":data[0]},"secret_key")
+            res.cookie(token)
+            jwt.verify(token,"secret_key",(err,result)=>{
+                res.json({massage:"Login Successful ",token:result})
+            })   
+        }
+        else{
+            res.send({massage: "Password is invalid.."})
+        }
+    }).catch((err)=>{
+        res.send(err)
+    })
+});
+
 
 module.exports = register;
