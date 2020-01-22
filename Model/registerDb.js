226a189f3db832bff547c87bd64e87f0765bc341
sAnjali12 <anjalisen060@gmail.com>
2020-01-22 16:20:17 +0530

    using havingIn  query by email

--- Model/registerDb.js
+++ Model/registerDb.js
@@ -4,5 +4,9 @@ var insertData = (userDetail)=>{
     return knex("registeration").insert(userDetail)
 }
 
+var user_login = (email)=>{
+    return knex.select('*').from('registeration').havingIn("email", email)
+    
+}
 
-module.exports={insertData}
+module.exports={insertData, user_login}
