1b56f491bc047b816fb00b1fea9706c5e3808ca6
sAnjali12 <anjalisen060@gmail.com>
2020-01-27 16:55:41 +0530

    change createPost table in that table add user_id

--- Model/createTable.js
+++ Model/createTable.js
@@ -14,29 +14,17 @@ var knex = require("./connection.js");
   
 
 
-// knex.schema.createTable('createPost', (table) => {
-//   table.increments('id')
-//   table.string('post')
-//   })
-// .then(() => console.log("table created"))
-//   .catch((err) => { console.log(err); throw err })
-//   .finally(() => {
-//       knex.destroy();
-// });
-
-
-knex.schema.createTable('user_likes', (table) => {
-  table.increments('id')
-  table.integer('user_id').unsigned();
-  table.foreign('user_id').references('registeration.id')
-  table.integer('post_id').unsigned();
-  table.foreign('post_id').references('createPost.post_id')
-  table.boolean('likes')
-  table.string('comment')
-})
+knex.schema.createTable('createPost', (table) => {
+  table.integer('user_id')
+  table.string('post_url')
+  table.string('Caption')
+  })
   .then(() => console.log("table created"))
   .catch((err) => { console.log(err); throw err })
   .finally(() => {
       knex.destroy();
 });
+
+
+
 
