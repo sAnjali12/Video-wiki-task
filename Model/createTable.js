39cb4c72a070629da6b7d7311a079276688cbddf
sAnjali12 <anjalisen060@gmail.com>
2020-01-29 17:23:06 +0530

    created tables for userDetails and userLikes

--- Model/createTable.js
+++ Model/createTable.js
@@ -1,30 +1,67 @@
 var knex = require("./connection.js");
 
-// knex.schema.createTable('registeration', (table) => {
-//     table.increments('id ')
-//     table.string('username')
-//     table.string('email')  
-//     table.string('password')
-//   })
-//   .then(() => console.log("table created"))
-//     .catch((err) => { console.log(err); throw err })
-//     .finally(() => {
-//         knex.destroy();
-// });
-  
+knex.schema.hasTable('registeration').then(function (exists) {
+  if (!exists) {
+  return knex.schema.createTable('registeration', (table) => {
+    table.increments('id ')
+    table.string('username')
+    table.string('email')  
+    table.string('password')
+    }) 
+} else {
+  console.log('registeration__table ALREADY EXIST!');
+   }
+})
 
 
-knex.schema.createTable('createPost', (table) => {
-  table.integer('user_id')
-  table.string('post_url')
-  table.string('Caption')
-  })
-  .then(() => console.log("table created"))
-  .catch((err) => { console.log(err); throw err })
-  .finally(() => {
-      knex.destroy();
-});
+knex.schema.hasTable('createPost').then(function (exists) {
+  if (!exists) {
+  return knex.schema.createTable('createPost', (table) => {
+      table.increments('post_id')
+      table.integer('user_id').unsigned();
+      table.foreign('user_id').references('registeration.id')
+      table.string('post_url')
+      table.string('caption')
+    }) 
+} else {
+  console.log('createPost__table ALREADY EXIST!');
+   }
+})
 
 
+knex.schema.hasTable('userLikes').then(function (exists) {
+  if (!exists) {
+  return knex.schema.createTable('userLikes', (table) => {
+    table.increments('id')
+    table.integer('user_id').unsigned();
+    table.foreign('user_id').references('registeration.id')
+    table.integer('post_id').unsigned();
+    table.foreign('post_id').references('createPost.post_id')
+    table.boolean('likes')
+    table.string('comment')
+})
+} else {
+  console.log('userLikes__table ALREADY EXIST!');
+   }
+})
+
+knex.schema.hasTable('userDetails').then(function (exists) {
+  if (!exists) {
+  return knex.schema.createTable('userDetails', (table) => {
+    table.increments('id')
+    table.string('Name')
+    table.integer('Birthday')
+    table.integer('Mobile')
+    table.string('Gender')
+    table.string('Location')
+    table.string('Education')
+    table.string('Experience') 
+    
+      
+})
+} else {
+  console.log('userDetails__table ALREADY EXIST!');
+   }
+})
 
 
